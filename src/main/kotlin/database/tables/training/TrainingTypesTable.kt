bb0486eca995d6b4edf4ab547bcb12f0c2ee6556
JugglypuffM <stas.klinin@gmail.com>
2024-12-27 18:17:26 +0500

    fix: поправлено повторное занесение типов тренировок при повторном запуске сервера

--- src/main/kotlin/database/tables/training/TrainingTypesTable.kt
+++ src/main/kotlin/database/tables/training/TrainingTypesTable.kt
@@ -4,5 +4,5 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 
 object TrainingTypesTable : IntIdTable() {
-    val fullName : Column<String> = varchar("name", 20)
+    val fullName : Column<String> = varchar("name", 20).uniqueIndex()
 }
